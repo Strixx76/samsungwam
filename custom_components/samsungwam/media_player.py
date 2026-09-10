4d061ba8d019a1b056300eedaf7533a9dc724e28
Strixx76 <17635047+Strixx76@users.noreply.github.com>
2026-09-10 11:38:57 +0000

    Fix volume bug

--- custom_components/samsungwam/media_player.py
+++ custom_components/samsungwam/media_player.py
@@ -233,7 +233,7 @@ class SamsungWamPlayer(WamEntity, MediaPlayerEntity):
     @property
     def volume_level(self) -> float | None:
         """Volume level of the media player (0..1)."""
-        if self.speaker.attribute.volume:
+        if self.speaker.attribute.volume is not None:
             return self.speaker.attribute.volume / 100
         return None
 
